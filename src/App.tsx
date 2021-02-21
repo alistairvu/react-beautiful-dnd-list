143f55e58b39ce3fd5d3966ccbf75239ca8a1328
Alistair Vu <alistairvu@gmail.com>
2021-02-21 11:24:17 +0700

    Initialize project using Create React App

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello!</h1>
+      <h2>Start editing the App.tsx file to begin work on your app.</h2>
+    </div>
+  )
+}
+
+export default App
